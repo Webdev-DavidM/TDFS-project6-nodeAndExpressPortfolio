147b02551e03d0460864edf5f7259bc240fd16e6
Webdev-DavidM <footballblubber@googlemail.com>
2020-06-22 10:47:02 +0100

    second commit port added

--- app.js
+++ app.js
@@ -61,6 +61,8 @@ app.use(errorHandler);
 
 // This makes the server listen so I can test it in the browser //
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log('listening on port 8080');
 });
